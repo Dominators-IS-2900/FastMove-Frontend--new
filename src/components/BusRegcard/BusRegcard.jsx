62f078667c881390c90359aca57a3f10aea96e4a
Dasuni <dasunichethanadc@gmail.com>
2023-04-13 13:18:06 +0530

    seat numbers based based on bus type with dropdown

--- src/components/BusRegcard/BusRegcard.jsx
+++ src/components/BusRegcard/BusRegcard.jsx
@@ -1,3 +1,95 @@
+// import React, { useState } from 'react';
+// import axios from 'axios';
+
+// const API_BASE_URL = 'http://localhost:5000';
+
+// const BusRegcard = () => {
+//   const [formData, setFormData] = useState({
+//     BusNo: '',
+//     BusType: '',
+//     NumOfSeats: '',
+//     LisenceRenewDate: '',
+//   });
+
+//   const handleChange = (e) => {
+//     const { name, value, type, files } = e.target;
+//     if (type === 'date') {
+//       setFormData({
+//         ...formData,
+//         [name]: new Date(value), // convert to a Date object
+//       });
+//     } else if (name === 'BusType') {
+//       const numSeats = (value === 'Normal') ? ['44', '49','54','59'] : ['100'];
+//       setFormData({
+//         ...formData,
+//         [name]: value,
+//         NumOfSeats: numSeats[0],
+//         numSeatsOptions: numSeats
+//       });
+      
+//     } else {
+//       setFormData({
+//         ...formData,
+//         [name]: files ? files[0] : value,
+//       });
+//     }
+//   };
+
+//   function displayInfo() {
+//     console.log(formData);
+//   }
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     const { BusNo, BusType, NumOfSeats, LisenceRenewDate } = formData;
+
+//     const formDataToSubmit = ({
+//       Bus_No: BusNo,
+//       Bus_type: BusType,
+//       No_ofSeats: NumOfSeats,
+//       Bus_Lisence_startDate: LisenceRenewDate
+//     });
+
+//     try {
+//       console.log(formDataToSubmit)
+//       axios.post(`${API_BASE_URL}/addBus`, formDataToSubmit).then((res) => {
+//         console.log(res.data);
+//       })
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   return (
+//     <div className='card shadow mb-4'>
+//       <div className='card-header py-3'>
+//         <h6 className='m-0 font-weight-bold text-primary'>Fill the Information </h6>
+//       </div>
+//       <div className='card-body'>
+//         <form onSubmit={handleSubmit}>
+//           <table className='table table-bordered' id='dataTable' width='100%' cellSpacing='0'>
+//             <tbody>
+//               <tr>
+//                 <td>1</td>
+//                 <td>Bus Number</td>
+//                 <td>
+//                   <input type='text' name='BusNo' id='BusNo' required onChange={handleChange} />
+//                 </td>
+//               </tr>
+//               <tr>
+//                 <td>2</td>
+//                 <td>Bus Type</td>
+//                 <td>
+//                   <select name='BusType' id='BusType' required onChange={handleChange}>
+//                     <option value=''>Select a type</option>
+//                     <option value='Normal'>Normal</option>
+//                     <option value='Semi Luxury'>Semi Luxury</option>
+//                     <option value='Luxury'>Luxury</option>
+//                   </select>
+//                 </td>
+//               </tr>
+//               <tr>
+
 import React, { useState } from 'react';
 import axios from 'axios';
 
@@ -13,15 +105,32 @@ const BusRegcard = () => {
 
   const handleChange = (e) => {
     const { name, value, type, files } = e.target;
-    if (type === 'radio') {
-      setFormData({
-        ...formData,
-        [name]: value,
-      });
-    } else if (type === 'date') {
+    if (type === 'date') {
       setFormData({
         ...formData,
         [name]: new Date(value), // convert to a Date object
+      });
+    } else if (name === 'BusType') {
+      let numSeats = [];
+      switch (value) {
+        case 'Normal':
+          numSeats = ['44', '49', '54', '59'];
+          break;
+        case 'Semi Luxury':
+          numSeats = ['54', '69'];
+          break;
+        case 'Luxury':
+          numSeats = ['30', '45'];
+          break;
+        default:
+          numSeats = [];
+          break;
+      }
+      setFormData({
+        ...formData,
+        [name]: value,
+        NumOfSeats: numSeats[0],
+        numSeatsOptions: numSeats,
       });
     } else {
       setFormData({
@@ -29,38 +138,28 @@ const BusRegcard = () => {
         [name]: files ? files[0] : value,
       });
     }
-
-    console.log(value)
   };
 
-  function displayInfo(){
+  function displayInfo() {
     console.log(formData);
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { BusNo, BusType, NumOfSeats, LisenceRenewDate } = formData;
-    
-  
-    const formDataToSubmit =( {
-      Bus_No:BusNo,
-      Bus_type:BusType,
-      No_ofSeats:NumOfSeats,
-      Bus_Lisence_startDate:LisenceRenewDate
+
+    const formDataToSubmit = ({
+      Bus_No: BusNo,
+      Bus_type: BusType,
+      No_ofSeats: NumOfSeats,
+      Bus_Lisence_startDate: LisenceRenewDate,
     });
 
     try {
-      console.log(formDataToSubmit)
-      axios.post(`${API_BASE_URL}/addBus`, formDataToSubmit
-      // , {
-      //   headers: {
-      //     'Content-Type': 'multipart/form-data',
-      //   },
-      // }
-      ).then((res)=>{
+      console.log(formDataToSubmit);
+      axios.post(`${API_BASE_URL}/addBus`, formDataToSubmit).then((res) => {
         console.log(res.data);
-
-      })
+      });
     } catch (error) {
       console.log(error);
     }
@@ -86,20 +185,23 @@ const BusRegcard = () => {
                 <td>2</td>
                 <td>Bus Type</td>
                 <td>
-                  <input type='radio' name='BusType' value='Normal' required onChange={handleChange} />
-                  <label htmlFor='Male'> Normal</label> <br />
-                  <input type='radio' name='BusType' value='Semi Luxury' required onChange={handleChange} />
-                  <label htmlFor='Female'> Semi Luxury</label>
-                  <br />
-                  <input type='radio' name='BusType' value='Luxury' required onChange={handleChange} />
-                  <label htmlFor='Female'> Luxury</label>
+                  <select name='BusType' id='BusType' required onChange={handleChange}>
+                    <option value=''>Select a type</option>
+                    <option value='Normal'>Normal</option>
+                    <option value='Semi Luxury'>Semi Luxury</option>
+                    <option value='Luxury'>Luxury</option>
+                  </select>
                 </td>
               </tr>
               <tr>
                 <td>3</td>
                 <td>Number of seats</td>
                 <td>
-                  <input type='text' name='NumOfSeats' id='NumOfSeats' required onChange={handleChange} />
+                  <select name='NumOfSeats' id='NumOfSeats' required onChange={handleChange}>
+                    {formData.numSeatsOptions && formData.numSeatsOptions.map(option => (
+                      <option value={option}>{option}</option>
+                    ))}
+                  </select>
                 </td>
               </tr>
               <tr>
@@ -107,6 +209,13 @@ const BusRegcard = () => {
                 <td>Lisence renew date</td>
                 <td>
                   <input type='date' name='LisenceRenewDate' id='LisenceRenewDate' required onChange={handleChange}/>
+                </td>
+              </tr>
+              <tr>
+                <td>4</td>
+                <td>Scanned copy of bus license</td>
+                <td>
+                  <input type='file' name='LisenceRenewDate' id='LisenceRenewDate' required onChange={handleChange}/>
                 </td>
               </tr>
             </tbody>
@@ -122,4 +231,6 @@ const BusRegcard = () => {
     </div>
   )}
 
-  export default BusRegcard;
+export default BusRegcard;
+
+
