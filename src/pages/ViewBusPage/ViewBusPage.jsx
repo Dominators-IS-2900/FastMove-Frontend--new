7cc84000fab9a9f86de6f8f768069241db8deef2
dasuni_wijethilaka <112398138+dasuni-wijethilaka@users.noreply.github.com>
2023-05-06 21:32:52 +0530

    Update ViewBusPage.jsx

--- src/pages/ViewBusPage/ViewBusPage.jsx
+++ src/pages/ViewBusPage/ViewBusPage.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import BusownerTopBar from '../../components/BusownertopBar/BusownerTopBar'
-import ArrivalTable from '../../components/ViewBusescard/ViewBusescard'
+import ViewBusescard from "../../components/ViewBusescard/ViewBusescard"
 import Footer from '../../components/footer/Footer'
+
 
 export default function ViewBus() {
  
@@ -9,7 +10,7 @@ export default function ViewBus() {
     <div id="content" style={{width: '1150px'}}>
         <div class="container-fluid mr-5" style={{width: '1100px'}}>
             <h1 class="h3 mb-2 text-gray-800">MY BUSES</h1>
-            <ArrivalTable/>
+            <ViewBusescard/>
         </div>
             <Footer/>
     </div>
