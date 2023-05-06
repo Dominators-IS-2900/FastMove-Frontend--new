d3cfbd72820bed501d79ccfab0bae6b2bfc721a7
hemanthawanniarachchi <hnwanniarachchi98@gmail.com>
2023-05-06 21:30:34 +0530

    fixing errors of viewBus page

--- src/components/BusownertopBar/BusownerTopBar.jsx
+++ src/components/BusownertopBar/BusownerTopBar.jsx
@@ -26,12 +26,12 @@ export default function BusownerTopBar() {
         <ul class="navbar-nav ml-auto">
              
             <li class="nav-item dropdown no-arrow mx-1">
-                <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button"
+                {/* <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="fas fa-envelope fa-fw"></i>
                     
                     <span class="badge badge-danger badge-counter">2</span>
-                </a>
+                </a> */}
 
                 {/* notification pop-up   */}
                 <div className='notify'>
